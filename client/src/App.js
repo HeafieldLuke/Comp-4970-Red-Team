833ffb55ee41ee9c12febbbd70d72026afc0331b
Luke Heafield <heafieldluke3@gmail.com>
2021-03-22 21:14:49 -0400

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header/Header'
+import Rooms from './components/Rooms/Rooms'
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Rooms />
+    </main>
+    
+  );
+}
+
+export default App;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import './header.css'
+
+const Header = () => {
+
+    return (
+        <ul>
+            <li><a>Rooms</a></li>
+            <li><a>Speakers</a></li>
+            <li><a>Time Slots</a></li>
+            <li><a>Sessions</a></li>
+        </ul>
+    );
+}
+
+export default Header;
